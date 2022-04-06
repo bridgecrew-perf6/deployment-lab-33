22abb3908e8648f5a907c2d93a5668e5a412f076
Harrison Kleiman <harrikleiman@gmail.com>
2022-04-06 15:47:50 -0400

    meme added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 })
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../styles.css"))
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)});
